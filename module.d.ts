fcd1c33c96efc14c3f3ccca3e10d6a89ed030e97
Sergey Koksharov <gentoosiastic@gmail.com>
2022-12-17 14:23:39 +0300

    fix: import images, css, json with correct type

--- module.d.ts
+++ module.d.ts
@@ -1,5 +1,29 @@
-declare module "*.css";
-declare module "*.jpg";
-declare module "*.png";
-declare module "*.svg";
-declare module "*.webp";
+declare module "*.css" {
+  const res: Record<string, string>;
+  export default res;
+}
+
+declare module "*.json" {
+  const res: string;
+  export default res;
+}
+
+declare module "*.jpg" {
+  const res: string;
+  export default res;
+}
+
+declare module "*.png" {
+  const res: string;
+  export default res;
+}
+
+declare module "*.svg" {
+  const res: string;
+  export default res;
+}
+
+declare module "*.webp" {
+  const res: string;
+  export default res;
+}
