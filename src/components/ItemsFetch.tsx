554966e679c7dbd9933cc68785052b2fb914ef45
Anastasia Sim <ASim4@slb.com>
2022-12-17 17:11:36 +0300

    feat: add error on fetch

--- src/components/ItemsFetch.tsx
+++ src/components/ItemsFetch.tsx
@@ -9,11 +9,15 @@ interface IAllCardsProps {
 
 export function AllCards({url, isSizeSmall}: IAllCardsProps) {
   const [items, setItems] = useState<IItem[]>([])
+  const [err, setErr] = useState(false)
   const fetchItems = () => {
     fetch(url)
-    .then(res =>res.json())
+    .then((res: Response) =>res.json())
     .then((json: IItemsDto) => {
       setItems(json.products)
+    })
+    .catch(error => {
+      setErr(true)
     })
   }
 
@@ -24,6 +28,7 @@ export function AllCards({url, isSizeSmall}: IAllCardsProps) {
   return (
     <div className="flex row-auto flex-wrap gap-5">
       {items.map(item => <ItemCard item={item} isSizeSmall = {isSizeSmall} key={item.id}/>)}
+      {err && <div className="p-20 text-center font-bold text-2xl">Error! No access to the server. Please try again later</div>}
     </div>
   );
 }
