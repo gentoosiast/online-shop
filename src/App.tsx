83fb4b292bfa9804261f87d803cddd7842697989
Sergey Koksharov <gentoosiastic@gmail.com>
2022-12-17 16:10:47 +0300

    feat: add temporary header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Header } from './components/Header';
 import { OrderForm } from './components/Order/OrderForm';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -7,6 +8,7 @@ import { IItem } from './types/IItem'; // TODO: temporary hack
 export function App() {
   return (
     <>
+      <Header />
       <div className="max-w-xs m-auto text-center text-emerald-700">
         Welcome aboard, best teammate on the planet Earth 🤗
       </div>
