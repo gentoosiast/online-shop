c6da4af5b75e3673947d4ffba2c2fd9203225b00
Anastasia Sim <ASim4@slb.com>
2022-12-23 12:18:25 +0300

    feat: change w objects

--- src/components/FilterPage.tsx
+++ src/components/FilterPage.tsx
@@ -7,6 +7,21 @@ import { ItemCardSize } from '../types/ItemCardSize';
 import { ItemCard } from "./ItemCard";
 import { SortOption } from "../types/SortOption";
 import featherSprite from 'feather-icons/dist/feather-sprite.svg';
+import { Sidebar } from "./Sidebar"
+
+export interface IFilters {
+  categories: string []
+  brands: string []
+  prices: number []
+  stock: number []
+}
+
+export interface IFilterApplied {
+  categories: boolean
+  brands: boolean
+  prices: boolean
+  stock: boolean
+}
 
 const isItemCardSize = (value: string): value is ItemCardSize => {
   return value === 'Small' || value === 'Large';
@@ -67,60 +82,133 @@ export const FilterPage = () => {
       <div className="preloader">Loading</div>
     );
   }
+  // const [tmp, setTmp] = useState(0)
+  // setTmp(items.length)
+  // const [initialParams, setInitialParams] = useState<IFilters>({categories: [''], brands: [''], prices: [0], stock: [0]})
+  // const [filteredParams, setFilteredParams] = useState<IFilters>({categories: [''], brands: [''], prices: [0], stock: [0]})
+  // const [isFiltered, setIsFiltered] = useState<IFilterApplied>({categories: false, brands: false, prices:false, stock: false});
+
+  // setInitialParams({
+  //   categories: [...new Set(items.map(el=> el.category))],
+  //   brands: [...new Set(items.map(el=> el.brand))],
+  //   prices: [...items.map(el=> el.price)],
+  //   stock: [...items.map(el=> el.stock)],
+  // })
+  // setFilteredParams({
+  //   categories: [...new Set(items.map(el=> el.category))],
+  //   brands: [...new Set(items.map(el=> el.brand))],
+  //   prices: [...items.map(el=> el.price)],
+  //   stock: [...items.map(el=> el.stock)],
+  // })
+
+
+  // const onFilterClick = (filterType: string, filterBox: string) => {
+  //   if (filterType === 'categories') {
+  //     // if (isFilteredByCategory) {
+  //     if (isFiltered.categories) {
+  //       // if (filteredCategories.includes(filterBox)) {
+  //       if (filteredParams.categories.includes(filterBox)) {
+  //         // if (filteredCategories.length === 1) {
+  //         if (filteredParams.categories.length === 1) {
+  //           // setIsFilteredByCategory(false);
+  //           setIsFiltered(prev => ({...prev, categories: false}))
+  //           // setFilteredCategories(initialParams.categories);
+  //           setFilteredParams(prev => ({ ...prev, categories: initialParams.categories}))
+  //         }
+  //         else {
+  //           // setFilteredCategories(prev => [...prev].filter(el => el !== filterBox))
+  //           setFilteredParams(prev => ({ ...prev, categories: [...prev.categories.filter(el => el !== filterBox)]}))
+  //         }
+  //       }
+  //       else {
+  //         // setFilteredCategories(prev => [...prev, filterBox])
+  //         setFilteredParams(prev => ({ ...prev, categories: [...prev.categories, filterBox]}))
+  //       }
+  //     } else {
+  //       // setIsFilteredByCategory(true);
+  //       setIsFiltered(prev => ({...prev, categories: true}))
+  //       // setFilteredCategories([filterBox])
+  //       setFilteredParams(prev => ({ ...prev, categories: [filterBox]}))
+  //     }
+  //   }
+
+  //   // if (filterType === 'brands') {
+  //   //   if (isFilteredByBrand) {
+  //   //     if (filteredBrands.includes(filterBox)) {
+  //   //       if (filteredBrands.length === 1) {
+  //   //         setIsFilteredByBrand(false);
+  //   //         setFilteredBrands(initialParams.brands)
+  //   //       }
+  //   //       else setFilteredBrands(prev => [...prev].filter(el => el !== filterBox))
+  //   //     }
+  //   //     else setFilteredBrands(prev => [...prev, filterBox])
+  //   //   } else {
+  //   //     setIsFilteredByBrand(true);
+  //   //     setFilteredBrands([filterBox])
+  //   //   }
+  //   // }
+  // }
+
+  // const itemsToRender = [...items].filter(elem => (filteredCategories.some(el => el === elem.category)))
+  // const itemsToRender = [...items].filter(elem => (filteredParams.categories.some(el => el === elem.category)))
+  // .filter(elem => filteredBrands.some(el => el === elem.brand))
 
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex justify-center items-center gap-10">
-        <div>
-          <select name="sort" defaultValue={initialSortOption} onChange={(event) => {
-            searchParams.set('sort', event.target.value);
-            setSearchParams(searchParams);
-          }}>
-            <option value="price-ASC">Sort by price (ascending)</option>
-            <option value="price-DESC">Sort by price (descending)</option>
-            <option value="rating-ASC">Sort by rating (ascending)</option>
-            <option value="rating-DESC">Sort by rating (descending)</option>
-            <option value="discount-ASC">Sort by discount (ascending)</option>
-            <option value="discount-DESC">Sort by discount (descending)</option>
-          </select>
+    <div>
+      {/* <Sidebar items={items} onCheck={(type, el)=>onFilterClick(type, el)} filters={initialParams}/> */}
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-center items-center gap-10">
+          <div>
+            <select name="sort" defaultValue={initialSortOption} onChange={(event) => {
+              searchParams.set('sort', event.target.value);
+              setSearchParams(searchParams);
+            }}>
+              <option value="price-ASC">Sort by price (ascending)</option>
+              <option value="price-DESC">Sort by price (descending)</option>
+              <option value="rating-ASC">Sort by rating (ascending)</option>
+              <option value="rating-DESC">Sort by rating (descending)</option>
+              <option value="discount-ASC">Sort by discount (ascending)</option>
+              <option value="discount-DESC">Sort by discount (descending)</option>
+            </select>
+          </div>
+          <div>Found: {items.length} items</div>
+          <Form id="search-form" role="search" autoComplete="off">
+            <input id="q" className="form-input" name="q" type='search' placeholder="I'm looking for..."
+              defaultValue={searchParams.get('q') ?? ''} aria-label="Search items"
+              onChange={(event) => {
+                event.preventDefault();
+                searchParams.set('q', event.target.value);
+                setSearchParams(searchParams);
+              }} />
+          </Form>
+          <div className="flex gap-2">
+            <button
+              onClick={() => {
+                searchParams.set('card', 'Small');
+                setSearchParams(searchParams);
+                setCardSize("Small");
+              }
+            }>
+              <svg className={`feather list-icon border-2 ${cardSize === "Small" ? 'border-slate-700': 'border-transparent'}`}>
+                <use href={`${featherSprite}#list`} />
+              </svg>
+            </button>
+            <button
+              onClick={() => {
+                searchParams.set('card', 'Large');
+                setSearchParams(searchParams);
+                setCardSize("Large");
+              }
+            }>
+              <svg className={`feather grid-icon border-2 ${cardSize === "Large" ? 'border-slate-700' : 'border-transparent'}`}>
+                <use href={`${featherSprite}#grid`} />
+              </svg>
+            </button>
+          </div>
         </div>
-        <div>Found: {items.length} items</div>
-        <Form id="search-form" role="search" autoComplete="off">
-          <input id="q" className="form-input" name="q" type='search' placeholder="I'm looking for..."
-            defaultValue={searchParams.get('q') ?? ''} aria-label="Search items"
-            onChange={(event) => {
-              event.preventDefault();
-              searchParams.set('q', event.target.value);
-              setSearchParams(searchParams);
-            }} />
-        </Form>
-        <div className="flex gap-2">
-          <button
-            onClick={() => {
-              searchParams.set('card', 'Small');
-              setSearchParams(searchParams);
-              setCardSize("Small");
-            }
-          }>
-            <svg className={`feather list-icon border-2 ${cardSize === "Small" ? 'border-slate-700': 'border-transparent'}`}>
-              <use href={`${featherSprite}#list`} />
-            </svg>
-          </button>
-          <button
-            onClick={() => {
-              searchParams.set('card', 'Large');
-              setSearchParams(searchParams);
-              setCardSize("Large");
-            }
-          }>
-            <svg className={`feather grid-icon border-2 ${cardSize === "Large" ? 'border-slate-700' : 'border-transparent'}`}>
-              <use href={`${featherSprite}#grid`} />
-            </svg>
-          </button>
+        <div className="flex flex-wrap justify-center gap-5">
+          {items.map(item => <ItemCard key={item.id} item={item} size={cardSize} />)}
         </div>
-      </div>
-      <div className="flex flex-wrap justify-center gap-5">
-        {items.map(item => <ItemCard key={item.id} item={item} size={cardSize} />)}
       </div>
     </div>
     )
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { IItem } from '../types/IItem';
+import styles from '../css/sidebar.module.css';
+import { IFilters } from './FilterPage';
+// import { Slider } from "./Slider"
+
+interface ISidebarProps {
+  items: IItem []
+  onCheck: (type: string, el: string)=> void;
+  filters: IFilters
+}
+
+export const Sidebar = ({items, onCheck, filters}: ISidebarProps) => {
+
+const pricesMinMax = [Math.min(...items.map(el=> el.price)), Math.max(...items.map(el=> el.price))]
+const stockMinMax = [Math.min(...items.map(el=> el.stock)), Math.max(...items.map(el=> el.stock))]
+const calcAmt = (arg1: number, arg2: string) => {
+  if (arg1 == 1) {
+    return items.filter(el=> el.category == arg2).length
+  }
+  return items.filter(el=> el.brand == arg2).length
+}
+
+const handleClick = (type: string, el:string) => {
+  onCheck(type, el);
+}
+
+  return (
+    <div className='w-fit'>
+      <div className={styles.box}>
+        <h3 className={styles.h3}>Category</h3>
+        <fieldset className={styles.items}>
+          {filters.categories.map((el, i) =>
+          <div key={i} className={styles.item} onClick={()=>handleClick('categories', el)}>
+            <input type='checkbox' id={'1'+i.toString()} className={styles.checkbox} onClick={(e)=>e.stopPropagation()}></input>
+            <label htmlFor={'1'+i.toString()}>{el}</label>
+            <span>({calcAmt(1, el)})</span>
+            </div>
+          )}
+        </fieldset>
+      </div>
+      <div className={styles.box}>
+        <h3 className={styles.h3}>Brand</h3>
+        <fieldset  className={styles.items}>
+          {filters.brands.map((el, i) =>
+            <div key={i} className={styles.item} onClick={()=>handleClick('brands', el)}>
+            <input type='checkbox' id={'2'+i.toString()} className={styles.checkbox} onClick={(e)=>e.stopPropagation()}></input>
+            <label htmlFor={'2'+i.toString()}>{el}</label>
+            <span>({calcAmt(2, el)})</span>
+            </div>
+          )}
+        </fieldset>
+      </div>
+      <div className={styles.box}>
+        <h3 className={styles.h3}>Price</h3>
+        <div className={styles.minMax}>
+          {pricesMinMax.map((el, i) => <input type='number' placeholder = {`${el}`} key={i} className='form-input w-44'
+          ></input>)}
+        </div>
+        <div id='slider'>
+            {/* <Slider min = {pricesMinMax[0]} max = {pricesMinMax[1]}></Slider> */}
+        </div>
+      </div>
+      <div className={styles.box}>
+        <h3 className={styles.h3}>Stock</h3>
+        <div className={styles.minMax}>
+          {stockMinMax.map((el, i) => <input type='number' placeholder = {`${el}`} key={i} className='form-input w-44'
+          ></input>)}
+        </div>
+        <div id='slider'>
+            {/* <Slider></Slider> */}
+        </div>
+      </div>
+    </div>
+  )
+}
