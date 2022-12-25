a1347155fb4b87f18c4e0f06e797cc7b99e36f38
Sergey Koksharov <gentoosiastic@gmail.com>
2022-12-25 13:31:11 +0300

    feat: add localStorage support for the shopping cart

--- src/storage/cart.store.ts
+++ src/storage/cart.store.ts
@@ -1,14 +1,83 @@
-import { makeObservable, observable, action, computed } from 'mobx';
+import { makeObservable, observable, action, computed, autorun } from 'mobx';
+import { PersistentStorage } from './persistentStorage';
 import { IItem } from '../types/IItem';
-import { CartItems, TPromo, TPromocodes } from '../types/cart';
+import { CartItem, CartItems, TPromo, TPromocodes } from '../types/cart';
 
 const availablePromos: TPromo[] = [['NOWAR', 15], ['RSSCHOOL', 5], ['NEWYEAR', 10]];
 
+const isIItem = (value: unknown): value is IItem => {
+  if (!(value && typeof value === 'object')) {
+    return false;
+  }
+
+  if ('id' in value && typeof value.id === 'number' &&
+      'title' in value && typeof value.title === 'string' &&
+      'description' in value && typeof value.description === 'string' &&
+      'price' in value && typeof value.price === 'number' &&
+      'discountPercentage' in value && typeof value.discountPercentage === 'number' &&
+      'rating' in value && typeof value.rating === 'number' &&
+      'stock' in value && typeof value.stock === 'number' &&
+      'brand' in value && typeof value.brand === 'string' &&
+      'category' in value && typeof value.category === 'string' &&
+      'thumbnail' in value && typeof value.thumbnail === 'string' &&
+      'images' in value && Array.isArray(value.images) &&
+      value.images.every((image) => typeof image === 'string')) {
+    return true;
+  }
+
+  return false;
+}
+
+const isCartItem = (value: unknown): value is CartItem => {
+  if (!(value && typeof value === 'object')) {
+    return false;
+  }
+
+  if ('item' in value && isIItem(value.item) &&
+      'quantity' in value && typeof value.quantity === 'number') {
+    return true;
+  }
+
+  return false;
+}
+
+type CartEntry = [number, CartItem];
+type CartEntries = CartEntry[];
+
+const isCartEntry = (value: unknown): value is CartEntry => {
+  if (!(value && Array.isArray(value))) {
+    return false;
+  }
+
+  if (value.length === 2 && typeof value[0] === 'number' && isCartItem(value[1])) {
+    return true;
+  }
+
+  return false;
+}
+
+const isCartEntries = (value: unknown): value is CartEntries => {
+  if (!(value && Array.isArray(value))) {
+    return false;
+  }
+
+  return value.every((el) => isCartEntry(el));
+}
+
 class CartStore {
-  items: CartItems = new Map();
+  items: CartItems;
   promos: TPromocodes = new Set();
+  storage: PersistentStorage;
 
   constructor() {
+    this.storage = new PersistentStorage('gentoosiast😺-sinastya🥇');
+    const storageCart = this.storage.get('cart');
+    if (isCartEntries(storageCart)) {
+      this.items = new Map(storageCart);
+    } else {
+      this.items = new Map();
+    }
+
     makeObservable(this, {
       items: observable,
       promos: observable.shallow,
@@ -118,3 +187,6 @@ export const cartStore = new CartStore();
 //     console.log(key, value);
 //   }
 // })
+autorun(() => {
+  cartStore.storage.set('cart', cartStore.items);
+});
