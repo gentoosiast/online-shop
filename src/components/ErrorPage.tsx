ff9bad6e82df3929d0f7473133b804477553443c
Anastasia Sim <sinastya@yandex.ru>
2023-01-07 22:02:57 +0300

    feat: styles for 404

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useRouteError, Link } from "react-router-dom";
+import cat404 from '../assets/cat404.png';
+
 
 type RouteError = {
   statusText?: string;
@@ -10,14 +12,17 @@ export const ErrorPage = () => {
   const error = useRouteError() as RouteError;
 
   return (
-    <div id="error-page" className="h-screen flex justify-center items-center">
-      <div className="flex flex-col items-center gap-4 text-center">
-        <h1 className="text-4xl font-bold">Oops!</h1>
-        <p>Sorry an unexpected error has occured.</p>
-        <p>
+    <div id="error-page" className="flex justify-center items-center">
+      <div className="flex flex-col items-center gap-4 text-center p-12">
+        <h1 className="text-8xl font-bold text-orange-900 p-4">404</h1>
+        <p>Похоже, мы не можем найти нужную Вам страницу</p>
+        {/* <p>
           <i>{error?.statusText || error?.message}</i>
-        </p>
-        <Link to="/" className="button button-add">Home</Link>
+        </p> */}
+        <div className=''>
+              <img src={cat404} alt="404 cat" className='w-64 p-8'/>
+            </div>
+        <Link to="/" className="button button-add">на главную</Link>
       </div>
     </div>
   );
